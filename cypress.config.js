d0367f1c2afde3e1d1be0e7515543616c50e15d1
Muhammad Feriansah Ramadhan <109209732+feriansyah237@users.noreply.github.com>
2025-02-24 17:22:53 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,14 +5,5 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {},
     baseUrl: "https://magento.softwaretestingboard.com/",
     defaultCommandTimeout: 60000,
-<<<<<<< HEAD
-=======
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl : 'https://magento.softwaretestingboard.com/'
->>>>>>> d4d8816 (upload checkout test script)
-=======
->>>>>>> 2eef7ed (small change)
   },
 });
